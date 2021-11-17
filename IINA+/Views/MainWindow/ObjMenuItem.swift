a0407798686e46e9350a59e022cbdc8d0c50c3c4
xjbeta <xjbeta.tiny@gmail.com>
2021-11-17 18:28:47 +0800

    ObjMenuItem.

---
+++ IINA+/Views/MainWindow/ObjMenuItem.swift
@@ -0,0 +1,13 @@
+//
+//  ObjMenuItem.swift
+//  IINA+
+//
+//  Created by xjbeta on 11/16/21.
+//  Copyright © 2021 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class ObjMenuItem: NSMenuItem {
+    var item: Any?
+}
