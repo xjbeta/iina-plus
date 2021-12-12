4f10fff4f0a8e22ee8db86d23f5cc29f647d2ee0
xjbeta <xjbeta.tiny@gmail.com>
2021-12-12 10:07:58 +0800

    Revert "Remove OpenFiles."
    This reverts commit d4ae9f36a2a6b284a61aa39c14bb1b05fe5377f6.

---
+++ IINA+/Views/Open Files/OpenFilesWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  OpenFilesWindowController.swift
+//  iina+
+//
+//  Created by xjbeta on 2019/6/11.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class OpenFilesWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        window?.isMovableByWindowBackground = true
+    }
+
+}
