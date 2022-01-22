078c704ca7469aaca6b75ea939a1199886128bfc
xjbeta <xjbeta.tiny@gmail.com>
2022-01-22 19:59:45 +0800

    Live state, site filter.

--- IINA+/NewViews/MainContentView.swift
+++ IINA+/NewViews/MainContentView.swift
@@ -9,11 +9,74 @@
 import SwiftUI
 
 struct MainContentView: View {
+    
     @FetchRequest(entity: Bookmark.entity(),
                   sortDescriptors: [],
                   predicate: nil,
                   animation: Animation.default
+    ) var allBookmarks: FetchedResults<Bookmark>
+    
+    @FetchRequest(entity: Bookmark.entity(),
+                  sortDescriptors: [NSSortDescriptor(key: #keyPath(Bookmark.order), ascending: true)],
+                  predicate: nil,
+                  animation: Animation.default
     ) var bookmarks: FetchedResults<Bookmark>
+    
+    
+    
+    
+    let liveState = MainViewController.LiveStateMenuItems.self
+    @State private var liveStateSelection: Int = 2
+    
+    var liveStatePicker: some View {
+        Picker("Live State", selection: $liveStateSelection) {
+            Text("All").tag(liveState.all.rawValue)
+            Divider()
+            Text("Living").tag(liveState.living.rawValue)
+            Text("Offline").tag(liveState.offline.rawValue)
+            Divider()
+            Text("Other").tag(liveState.other.rawValue)
+        }
+        .onChange(of: liveStateSelection) { _ in
+            self.updateFilter()
+        }
+    }
+    
+    @State private var siteSelection: String = "All"
+    
+    var sites: [SupportSites] {
+        get {
+            let sites = allBookmarks.map {
+                $0.url
+            }.compactMap(SupportSites.init(url: ))
+                .filter {
+                    $0 != .unsupported
+                }
+            
+            return Array(Set(sites))
+                .sorted {
+                    $0.siteName > $1.siteName
+                }
+        }
+    }
+    
+    
+    var sitePicker: some View {
+        Picker("Site", selection: $siteSelection) {
+            Text("All Sites").tag("All")
+            Divider()
+            ForEach(sites, id: \.rawValue) {
+                Text($0.siteName).tag($0.rawValue)
+            }
+        }
+        .onChange(of: siteSelection) { _ in
+            self.updateFilter()
+        }
+    }
+    
+    
+    
+
     
     var bookmarksView: some View {
         LazyVGrid(columns: gridItemLayout, spacing: 5) {
@@ -43,6 +106,63 @@ struct MainContentView: View {
                 .padding()
         }
         .frame(minWidth: 300, maxWidth: .infinity)
+        .toolbar {
+            liveStatePicker
+            sitePicker
+        }
+    }
+    
+    func updateFilter() {
+        var f = ""
+        
+        switch liveState.init(rawValue: liveStateSelection) {
+        case .all:
+            f = ""
+        case .living:
+            f = "state == 1"
+        case .offline:
+            f = "state == 0"
+        case .other:
+            // Bangumi bilibili -99
+            f = "state == -1"
+        case .none:
+            f = ""
+        }
+        
+        
+        var f2 = ""
+        
+        
+        
+        switch siteSelection {
+        case "All":
+            f2 = ""
+        default:
+            f2 = "url CONTAINS '\(siteSelection)'"
+            if siteSelection == SupportSites.bilibili.rawValue
+                || siteSelection == SupportSites.bangumi.rawValue {
+                f = ""
+            }
+        }
+
+        let format = [f, f2].filter {
+            $0 != ""
+        }.joined(separator: " && ")
+        
+        guard format != "" else {
+            bookmarks.nsPredicate = nil
+            Log("Remove Filters")
+            return
+        }
+
+        if let oldF = bookmarks.nsPredicate?.predicateFormat,
+            oldF == format {
+            return
+        }
+        
+        Log("New Filters: \(format)")
+        let p = NSPredicate(format: format)
+        bookmarks.nsPredicate = p
     }
 }
 
