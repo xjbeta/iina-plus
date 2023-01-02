e03fe35abe5e5ed6b651fa62d471320fd33bfadc
xjbeta <xjbeta.tiny@gmail.com>
2023-01-02 11:20:01 +0800

    fix: douyin dm

--- IINA+/Utils/Danmaku/DouYinDM.swift
+++ IINA+/Utils/Danmaku/DouYinDM.swift
@@ -10,6 +10,8 @@ import Cocoa
 import WebKit
 import Alamofire
 import PromiseKit
+import SocketRocket
+import Gzip
 
 class DouYinDM: NSObject {
     var url = ""
@@ -28,6 +30,8 @@ class DouYinDM: NSObject {
     
     var roomId = ""
     
+    var socket: SRWebSocket?
+    
     private let tokenString = "bXNUb2tlbg==".base64Decode()
     private var webview: WKWebView? = WKWebView()
     private var requestTimer: Timer?
@@ -35,6 +39,38 @@ class DouYinDM: NSObject {
     var privateKeys: [String] {
         proc.videoDecoder.douyin.privateKeys
     }
+    
+    func initWS() {
+        
+        let ws = "d3NzOi8vd2ViY2FzdDMtd3Mtd2ViLWhsLmRvdXlpbi5jb20vd2ViY2FzdC9pbS9wdXNoL3YyLz9hcHBfbmFtZT1kb3V5aW5fd2ViJnZlcnNpb25fY29kZT0xODA4MDAmd2ViY2FzdF9zZGtfdmVyc2lvbj0xLjMuMCZ1cGRhdGVfdmVyc2lvbl9jb2RlPTEuMy4wJmNvbXByZXNzPWd6aXAmaG9zdD1odHRwczovL2xpdmUuZG91eWluLmNvbSZhaWQ9NjM4MyZsaXZlX2lkPTEmZGlkX3J1bGU9MyZkZWJ1Zz10cnVlJmVuZHBvaW50PWxpdmVfcGMmc3VwcG9ydF93cmRzPTEmaW1fcGF0aD0vd2ViY2FzdC9pbS9mZXRjaC8mZGV2aWNlX3BsYXRmb3JtPXdlYiZjb29raWVfZW5hYmxlZD10cnVlJmJyb3dzZXJfbGFuZ3VhZ2U9ZW4tVVMmYnJvd3Nlcl9wbGF0Zm9ybT1NYWNJbnRlbCZicm93c2VyX29ubGluZT10cnVlJnR6X25hbWU9QXNpYS9TaGFuZ2hhaSZpZGVudGl0eT1hdWRpZW5jZSZoZWFydGJlYXREdXJhdGlvbj0xMDAwMCZyb29tX2lkPQ==".base64Decode() + "\(roomId)"
+        
+        guard let u = URL(string: ws) else { return }
+        var req = URLRequest(url: u)
+        let cookieString = cookies.map {
+            "\($0.key)=\($0.value)"
+        }.joined(separator: ";")
+        
+        req.setValue(cookieString, forHTTPHeaderField: "Cookie")
+        req.setValue("https://live.douyin.com", forHTTPHeaderField: "referer")
+        req.setValue(ua, forHTTPHeaderField: "User-Agent")
+        
+        socket?.delegate = nil
+        socket?.close()
+        socket = nil
+        
+        socket = SRWebSocket(urlRequest: req)
+        socket?.delegate = self
+        socket?.open()
+        
+        requestTimer = Timer.scheduledTimer(withTimeInterval: 10, repeats: true) {_ in
+            
+            var pf = DouYinPushFrame()
+            pf.payloadType = "hb"
+            
+            try? self.socket?.sendPing(pf.serializedData())
+        }
+    }
+    
     
     func start(_ url: String) {
         self.url = url
@@ -111,75 +147,13 @@ class DouYinDM: NSObject {
         }
     }
     
-    func requestDM(_ url: String) -> Promise<(Response, Int)> {
-        Promise { resolver in
-            let date = Date()
-            let cookieString = cookies.map {
-                "\($0.key)=\($0.value)"
-            }.joined(separator: ";")
-            
-            let headers = HTTPHeaders([
-                "User-Agent": ua,
-                "referer": "https://live.douyin.com",
-                "Cookie": cookieString
-            ])
-            
-            AF.request(url, headers: headers).response {
-                if let newToken = $0.response?.headers.filter ({
-                    $0.name == "eC1tcy10b2tlbg==".base64Decode()
-                }).first?.value {
-                    self.cookies[self.tokenString] = newToken
-                }
-                
-                guard let data = $0.data else {
-                    resolver.reject(VideoGetError.notFountData)
-                    return
-                }
-                
-                let rtt = Int(abs(date.timeIntervalSinceNow * 1000))
-                do {
-                    let re = try Response(serializedData: data)
-                    resolver.resolve((re, rtt), $0.error)
-                } catch let error {
-                    resolver.reject(error)
-                }
-            }
-        }
-    }
-    
-    func decodeDM(_ result: (Response, Int)) {
-        let re = result.0
-        let lastRtt = "\(result.1)"
-        let internalExt = re.internalExt.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-        let cursor = re.cursor
-        
-        let msgs = re.messages.filter {
-            $0.method == "WebcastChatMessage"
-        }.compactMap {
-            try? ChatMessage(serializedData: $0.payload)
-        }
-        
-//        Log(msgs.map({ $0.content }))
-        
-        msgs.forEach {
-            delegate?.send(.init(method: .sendDM, text: $0.content))
-        }
-        
-        requestTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { [weak self] timer in
-            guard let self = self else { return }
-            self.prepareURL(internalExt: internalExt, cursor: cursor, lastRtt: lastRtt).then {
-                self.requestDM($0)
-            }.done {
-                self.decodeDM($0)
-            }.catch {
-                Log($0)
-            }
-        }
-    }
     
     func stop() {
         requestTimer?.invalidate()
         requestTimer = nil
+        socket?.close()
+        socket = nil
+        
         DispatchQueue.main.async {
             self.webview?.stopLoading()
             self.webview = nil
@@ -193,12 +167,8 @@ class DouYinDM: NSObject {
     func startRequests() {
         getRoomId().then {
             self.prepareCookies()
-        }.then {
-            self.prepareURL()
-        }.then {
-            self.requestDM($0)
         }.done {
-            self.decodeDM($0)
+            self.initWS()
         }.catch {
             Log($0)
         }
@@ -208,5 +178,74 @@ class DouYinDM: NSObject {
 extension DouYinDM: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         startRequests()
+    }
+}
+
+extension DouYinDM: SRWebSocketDelegate {
+    func webSocketDidOpen(_ webSocket: SRWebSocket) {
+        Log("webSocketDidOpen")
+        
+    }
+    
+    func webSocket(_ webSocket: SRWebSocket, didCloseWithCode code: Int, reason: String?, wasClean: Bool) {
+        Log("webSocketdidClose \(reason ?? "")")
+    }
+    
+    func webSocket(_ webSocket: SRWebSocket, didReceiveMessageWith data: Data) {
+        do {
+            let re = try DouYinResponse(serializedData: data)
+            let ree = try DouYinDMResponse(serializedData: re.data.gunzipped())
+            
+            ree.messages.filter {
+                $0.method == "WebcastChatMessage"
+            }.compactMap {
+                try? ChatMessage(serializedData: $0.payload)
+            }.forEach {
+                delegate?.send(.init(method: .sendDM, text: $0.content))
+            }
+            
+            guard ree.needAck else { return }
+            
+            var pf = DouYinPushFrame()
+            pf.payloadType = "ack"
+            pf.logid = re.wssPushLogID
+            
+            let payload: [UInt8] = {
+                var t = [UInt8]()
+                func push(_ e: UInt32) {
+                    t.append(UInt8(e))
+                }
+                
+                ree.internalExt.unicodeScalars.forEach {
+                    let e = $0.value
+                    switch e {
+                    case _ where e < 128:
+                        push(e)
+                    case _ where e < 2048:
+                        push(192 + (e >> 6))
+                        push(128 + (63 & e))
+                    case _ where e < 65536:
+                        push(224 + (e >> 12))
+                        push(128 + (e >> 6 & 63))
+                        push(128 + (63 & e))
+                    default:
+                        break
+                    }
+                }
+                
+                return t
+            }()
+            
+            pf.data = Data(payload)
+            
+            try? webSocket.send(data: pf.serializedData())
+            
+        } catch let error {
+            Log("\(error)")
+        }
+    }
+    
+    func webSocket(_ webSocket: SRWebSocket, didFailWithError error: Error) {
+        Log(error)
     }
 }
