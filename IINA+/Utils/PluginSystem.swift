d4dabab499c9a57b9a290328fb47dcae8d16745a
xjbeta <xjbeta.tiny@gmail.com>
2023-01-20 14:50:29 +0800

    Check danmaku plugin

--- IINA+/Utils/PluginSystem.swift
+++ IINA+/Utils/PluginSystem.swift
@@ -17,13 +17,7 @@ class PluginSystem: NSObject {
         case fileNotFound(String)
     }
     
-    static func shouldInstall() throws -> Bool {
-        let url = try pluginURL()
-        
-        
-    }
-    
-    static func pluginVersion() throws -> Int {
+    static func pluginVersion() throws -> String {
         struct Info: Decodable {
             let version: String
             let identifier: String
@@ -43,12 +37,18 @@ class PluginSystem: NSObject {
             throw PluginError.versionNotFound
         }
         
-        let vs = info.version.split(separator: ".").compactMap({ Int($0) })
+        return info.version
+    }
+    
+    static func pluginNumberVersion() throws -> Int {
+        let version = try pluginVersion()
+        
+        let vs = version.split(separator: ".").compactMap({ Int($0) })
         guard vs.count == 3 else {
             throw PluginError.versionFormatError
         }
         
-        return vs[0] * 10^6 + vs[1] * 10^3 + vs[2]
+        return vs[0] * 1_000_000 + vs[1] * 1_000 + vs[2]
     }
     
     static func pluginURL() throws -> URL {
@@ -56,21 +56,23 @@ class PluginSystem: NSObject {
         
         var path = try FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
         path.appendPathComponent("com.colliderli.iina")
+        path.appendPathComponent("plugins")
         path.appendPathComponent("com.xjbeta.danmaku.iinaplugin")
         return path
     }
     
+    /*
     static func installPlugin() throws {
         let fm = FileManager.default
         let path = Bundle.main.path(forResource: "iina-plugin-danmaku", ofType: "iinaplgz")
-        
+
         let pluginUrl = try pluginURL()
-        
+
         guard fm.fileExists(atPath: plgzPath) else {
             throw PluginError.fileNotFound(path ?? "nil path")
         }
-        
-        
+
+
         let pluginFolder = pluginUrl.deletingLastPathComponent()
         if !fm.fileExists(atPath: pluginFolder.path) {
             try fm.createDirectory(at: pluginFolder, withIntermediateDirectories: true)
@@ -80,4 +82,5 @@ class PluginSystem: NSObject {
         }
         try fm.copyItem(atPath: plgzPath, toPath: pluginUrl.path)
     }
+     */
 }
