ce383eea2135f0211100669817c3b14799782af3
xjbeta <xjbeta.tiny@gmail.com>
2023-03-10 13:17:52 +0800

    new api

--- IINA+/Utils/VideoDecoder/KuaiShou.swift
+++ IINA+/Utils/VideoDecoder/KuaiShou.swift
@@ -13,14 +13,13 @@ import PMKAlamofire
 import Marshal
 
 class KuaiShou: NSObject, SupportSiteProtocol {
-    lazy var session: Session = {
-        let configuration = URLSessionConfiguration.af.default
-        let ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15"
-        configuration.headers.add(.userAgent(ua))
-        return Session(configuration: configuration)
-    }()
-    
-    
+
+	enum KuaiShouError: Error {
+		case invalidLink
+		case nilReferer
+		case apiLimited
+	}
+	
     func liveInfo(_ url: String) -> Promise<LiveInfo> {
         getInfo(url).map {
             $0
@@ -29,20 +28,48 @@ class KuaiShou: NSObject, SupportSiteProtocol {
     
     func decodeUrl(_ url: String) -> Promise<YouGetJSON> {
         getInfo(url).map {
-            $0.write(to: YouGetJSON(url: url))
+            $0.write(to: YouGetJSON(rawUrl: url))
         }
     }
     
     func getInfo(_ url: String) -> Promise<KuaiShouInfo> {
-        AF.request(url).responseString().map {
-            $0.string
-                .subString(from: "window.__INITIAL_STATE__=", to: ";(")
-                .data(using: .utf8) ?? Data()
-        }.map {
-            let obj = try JSONParser.JSONObjectWithData($0)
-            let info = try KuaiShouInfo(object: obj)
-            return info
-        }
+		guard let uc = URLComponents.init(string: url),
+			  uc.path.starts(with: "/u/") else {
+			return .init(error: KuaiShouError.invalidLink)
+		}
+		
+		let eid = String(uc.path.dropFirst(3))
+		
+		let pars: Parameters = [
+			"eid": eid,
+			"clientType": "WEB_OUTSIDE_SHARE_H5",
+			"shareMethod": "card",
+			"source": 6
+		]
+		
+		var headers: HTTPHeaders = [
+			.userAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1"),
+		]
+		
+		return AF.request(url, headers: headers).responseData().done {
+			guard let ref = $0.response.response?.url?.absoluteString else {
+				throw KuaiShouError.nilReferer
+			}
+			headers.add(name: "Referer", value: ref)
+		}.then {
+			AF.request(
+				"https://livev.m.chenzhongtech.com/rest/k/live/byUser?kpn=GAME_ZONE",
+				method: .post,
+				parameters: pars,
+				encoding: JSONEncoding.default,
+				headers: headers).responseData()
+		}.map {
+			let obj = try JSONParser.JSONObjectWithData($0.data)
+			let result: Int = try obj.value(for: "result")
+			guard result == 1 else { throw KuaiShouError.apiLimited }
+			let info = try KuaiShouInfo(object: obj)
+			return info
+		}
     }
 }
 
@@ -57,38 +84,35 @@ struct KuaiShouInfo: Unmarshaling, LiveInfo {
     
     var site: SupportSites = .kuaiShou
     
-    var representations: [Representation] = []
+    var playUrls: [PlayUrl] = []
     
     init(object: Marshal.MarshaledObject) throws {
-        name = try object.value(for: "liveroom.author.name")
-        avatar = try object.value(for: "liveroom.author.avatar")
-        title = try object.value(for: "liveroom.liveStream.caption")
-        cover = try object.value(for: "liveroom.liveStream.coverUrl")
-        isLiving = try object.value(for: "liveroom.isLiving")
-        
-        let playUrls: [PlayUrl] = try object.value(for: "liveroom.liveStream.playUrls")
-        
-        representations = playUrls.first?.representations ?? []
+        name = try object.value(for: "liveStream.user.user_name")
+        avatar = try object.value(for: "liveStream.user.headurl")
+        title = try object.value(for: "liveStream.caption")
+        cover = try object.value(for: "liveStream.coverUrl")
+        isLiving = try object.value(for: "liveStream.living")
+		playUrls = try object.value(for: "liveStream.multiResolutionHlsPlayUrls")
     }
     
     struct PlayUrl: Unmarshaling {
-        var representations: [Representation]
+		var name: String
+		var level: Int
+		var urls: [String]
         
         init(object: Marshal.MarshaledObject) throws {
-            representations = try object.value(for: "adaptationSet.representation")
+			name = try object.value(for: "name")
+			level = try object.value(for: "level")
+			let urls: [Url] = try object.value(for: "urls")
+			self.urls = urls.map {
+				$0.url
+			}
         }
     }
     
-    struct Representation: Unmarshaling {
-        var bitrate: Int
-        var level: Int
-        var name: String
+    struct Url: Unmarshaling {
         var url: String
-        
         init(object: Marshal.MarshaledObject) throws {
-            bitrate = try object.value(for: "bitrate")
-            level = try object.value(for: "level")
-            name = try object.value(for: "name")
             url = try object.value(for: "url")
         }
     }
@@ -97,12 +121,13 @@ struct KuaiShouInfo: Unmarshaling, LiveInfo {
         var json = yougetJson
         json.title = title
         
-        representations.forEach {
-            var stream = Stream(url: $0.url)
-            stream.quality = $0.bitrate
-            json.streams[$0.name] = stream
-        }
-        
+		playUrls.forEach {
+			guard let url = $0.urls.first else { return }
+			var stream = Stream(url: url)
+			stream.quality = $0.level
+			json.streams[$0.name] = stream
+		}
+		
         return json
     }
 }
