7801ac5b88e4d3081f98e1de125f6f25326a5e75
xjbeta <xjbeta.tiny@gmail.com>
2024-11-17 16:40:55 +0800

    misc: douyu pars

--- IINA+/Utils/VideoDecoder/Douyu.swift
+++ IINA+/Utils/VideoDecoder/Douyu.swift
@@ -144,13 +144,13 @@ actor Douyu: SupportSiteProtocol {
         
         let pars = ["v": v,
                     "did": didStr,
-                    "tt": time,
+                    "tt": "\(time)",
                     "sign": sign.subString(from: "sign="),
                     "cdn": "ali-h5",
                     "rate": "\(rate)",
                     "ver": "Douyu_221111905",
                     "iar": "0",
-                    "ive": "0"] as [String : Any]
+                    "ive": "0"]
         
 		let url = "https://www.douyu.com/lapi/live/getH5Play/\(roomID)"
 		let data = try await AF.request(url, method: .post, parameters: pars).serializingData().value
